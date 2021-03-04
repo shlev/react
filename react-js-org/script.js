1a9ff9e85ef0007768eb042d0d819630872cc68e
shlomi <shlomi@magic-flex.com>
2021-03-04 18:00:15 +0200

    component-props (extracting components)

--- react-js-org/script.js
+++ react-js-org/script.js
@@ -6,24 +6,35 @@ function formatDate(date) {
     return date.toLocaleDateString();
   }
 
+function Avatar(props) {
+    return (
+        <img className="Avatar" 
+                src={props.user.avatarUrl}
+                alt={props.user.name}     
+            />
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user = {props.user} />
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
 function Comment(props) {
     return (
     <div className="Comment">
-        <div className="UserInfo">
-            <img className="Avatar" 
-                src={props.author.avatarUrl}
-                alt={props.author.name}     
-            />
-            <div className="UserInfo-name">
-                {props.author.name}
-            </div>
-            <div className="Common-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-
+        <UserInfo user={props.author}/>
+        <div className="Common-text">
+            {props.text}
+        </div>
+        <div className="Comment-date">
+            {formatDate(props.date)}
         </div>
     </div>
     );
