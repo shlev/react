326a01475991d3a74669cb78d2ddf6996c215814
shlomi <shlomi@magic-flex.com>
2021-03-04 16:48:29 +0200

    introducing jsx

--- react-js-org/script.js
+++ react-js-org/script.js
@@ -1,4 +1,23 @@
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Key',
+    lastName: 'Sar'
+};
+
+const element = <h1>Hello, {formatName(user)}</h1>
 ReactDOM.render(
-    <h1>Hello, World</h1>,
+    element,
     document.getElementById('root')
 )
+
+
+/*
+JSX Attributes
+const element = <div tabIndex="0"></div>;
+
+const element = <img src={user.avatarUrl}></img>;
+
+*/
