dd02bdb5b9d4e65fd34e4ae9baa2d21e63a7dd44
shlomi <shlomi@magic-flex.com>
2021-03-04 17:48:33 +0200

    component-props

--- react-js-org/script.js
+++ react-js-org/script.js
@@ -1,20 +1,65 @@
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
-
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    )
-    
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
 }
 
+function formatDate(date) {
+    return date.toLocaleDateString();
+  }
 
-setInterval(tick, 1000);  //Just for example , React mostly Render once.
+function Comment(props) {
+    return (
+    <div className="Comment">
+        <div className="UserInfo">
+            <img className="Avatar" 
+                src={props.author.avatarUrl}
+                alt={props.author.name}     
+            />
+            <div className="UserInfo-name">
+                {props.author.name}
+            </div>
+            <div className="Common-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+
+        </div>
+    </div>
+    );
+}
+
+function App(props) {
+    return (
+        <div>
+            <Welcome name="KeySar" />
+            <Welcome name="Jose" />
+            <Welcome name="Lidia" />
+            <Comment 
+                date={props.comment.date}
+                text={props.comment.text}
+                author={comment.author}
+                />
+        </div>
+    );
+}
+
+const element = <Welcome name="Key Sar" />
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+  };
+
+ReactDOM.render(
+    <App comment = {comment}/>,
+    document.getElementById('root')
+)
+
 
 
 /*
