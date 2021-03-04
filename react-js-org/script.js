70fa2bed9848944a471c36123531aa49f0998417
shlomi <shlomi@magic-flex.com>
2021-03-04 17:09:57 +0200

    rendering-elements

--- react-js-org/script.js
+++ react-js-org/script.js
@@ -1,23 +1,23 @@
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
+
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    )
+    
 }
 
-const user = {
-    firstName: 'Key',
-    lastName: 'Sar'
-};
 
-const element = <h1>Hello, {formatName(user)}</h1>
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-)
+setInterval(tick, 1000);  //Just for example , React mostly Render once.
 
 
 /*
-JSX Attributes
-const element = <div tabIndex="0"></div>;
 
-const element = <img src={user.avatarUrl}></img>;
-
+React update only necessary elements that was changed.
 */
