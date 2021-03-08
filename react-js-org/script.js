ca930767b4613e23bd53c56a2104afbc06117ac5
shlomi <shlomi@magic-flex.com>
2021-03-08 12:31:08 +0200

    conditional rendering 2

--- react-js-org/script.js
+++ react-js-org/script.js
@@ -66,7 +66,45 @@ class LoginControl extends React.Component {
     }
 }
 
+function WarningBanner(props) {
+    if (!props.warn) {
+        return null;
+    }
+
+    return (
+        <div className="warning">Warning!</div>
+    );
+}
+class Page extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { showWarning: true };
+        this.handleToggleClick = this.handleToggleClick.bind(this);
+    }
+
+    handleToggleClick() {
+        this.setState(state => ({ 
+            showWarning: !state.showWarning
+        }));
+    }
+    render() {
+        return (
+            <div>
+                <WarningBanner warn={this.state.showWarning}/>
+                <button onClick={this.handleToggleClick}>
+                    {this.state.showWarning ? 'Hide' : 'Show'}
+                </button>
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <LoginControl  />,
+    <div>
+        <LoginControl  />
+        <Page />
+    </div>,
+    
     document.getElementById('root')
 )
